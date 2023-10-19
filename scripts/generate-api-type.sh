1a98a856794173e27793c8559755b4bdc41551c4
Alexandre Bordin <alexandre.bordin@spacefill.fr>
2023-10-19 17:53:29 +0200

    refactor(scripts): add var to set openapi file

--- scripts/generate-api-type.sh
+++ scripts/generate-api-type.sh
@@ -3,4 +3,6 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-npx openapi-client-axios-typegen https://dash.readme.com/api/v1/api-registry/p570f12ln978a7s > src/api/spacefill-api-openapi.d.ts
+OPENAPI_DEF="https://dash.readme.com/api/v1/api-registry/p570f12ln978a7s"
+
+npx openapi-client-axios-typegen "$OPENAPI_DEF" > src/api/spacefill-api-openapi.d.ts
