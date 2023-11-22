0a3f59732e5c28617c093fa1dd54526f54714721
Alexandre Bordin <alexandre.bordin@spacefill.fr>
2023-11-22 12:42:49 +0100

    tests: add tests for Spacefill api wrapper #3

---
+++ tests/ci/api/SpacefillAPIWrapperV1.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import MockAdapter from 'axios-mock-adapter';
+
+import { SpacefillAPIWrapperV1 } from '../../../src/api/SpacefillAPIWrapperV1.ts';
+import { WorkflowType } from '../../../src/api/APIContext.ts';
+import { beforeEach } from 'mocha';
+import initTestEnv from '../../testUtils/initTestEnv.ts';
+
+describe('SpacefillAPIWrapperV1', () => {
+  let sdk: SpacefillAPIWrapperV1;
+
+  before(() => initTestEnv())
+
+  beforeEach(async () => {
+    process.env.LOG_LEVEL = 'fatal'; // set to 'debug' to see request logs.
+
+    sdk = new SpacefillAPIWrapperV1();
+    await sdk.initClient(
+      'http://127.0.0.1:5004',
+      'testBearer',
+      WorkflowType.EXPORT_ITEM_REFERENCES
+    );
+  });
+
+  it('should get ping', async () => {
+    const axiosInstance = sdk.getAxiosInstance();
+
+    const mock = new MockAdapter(axiosInstance, { onNoMatch: "throwException" });
+    mock.onGet("/v1/ping", {}).reply(200, { data: 'mocked response' });
+
+    await sdk.client.get_v1_ping();
+
+    expect(mock.history.get.length).to.equals(1);
+  });
+});
